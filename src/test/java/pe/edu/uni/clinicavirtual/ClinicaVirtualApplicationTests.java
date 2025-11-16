c12ad2240d988b1c4e8d13cd7bc82dd59f5e98cc
FrankDaniel-322 <frank.chuctaya.q@uni.pe>
2025-11-16 01:00:43 -0500

    Initial commit: Spring Boot project setup

---
+++ src/test/java/pe/edu/uni/clinicavirtual/ClinicaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.uni.clinicavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
