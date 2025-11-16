53ecfe0110efd34e7b2e0272a2cafd2ea4b145a5
FrankDaniel-322 <frank.chuctaya.q@uni.pe>
2025-11-16 11:13:38 -0500

    Cambio 4

--- src/main/java/pe/edu/uni/clinicavirtual/ClinicaVirtualApplication.java
+++ src/main/java/pe/edu/uni/clinicavirtual/ClinicaVirtualApplication.java
@@ -9,5 +9,12 @@ public class ClinicaVirtualApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaVirtualApplication.class, args);
 	}
-
 }
+
+//CHAT
+// lito ahora si ya jeffer no c va a botar diciendo no quiero descargar nd V: igual toy viendo como activo el VS segun yo deberia tenerlo en algun lado XD sino por mientras asi
+// tienes wsp?  como no tienes? XD o quieres k yo le diga no c te entendio v:vvv 
+// Nada XD no le digas Nada. Primero hagamos funcional esto. Espero q no dure solo 30 minutos
+
+// LA SOLUCION PARA MANDAR LOS CAMBIOS AL GITHUB ES EL SIGUIENTE:
+// 
