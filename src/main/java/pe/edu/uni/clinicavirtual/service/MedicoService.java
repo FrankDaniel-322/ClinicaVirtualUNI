17cc0e7d19f88f5b6498ce76d3af6488d9ed79d8
FrankDaniel-322 <frank.chuctaya.q@uni.pe>
2025-11-17 03:15:29 -0500

    xd

--- src/main/java/pe/edu/uni/clinicavirtual/service/MedicoService.java
+++ src/main/java/pe/edu/uni/clinicavirtual/service/MedicoService.java
@@ -9,7 +9,9 @@ import pe.edu.uni.clinicavirtual.repository.ConsultaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
